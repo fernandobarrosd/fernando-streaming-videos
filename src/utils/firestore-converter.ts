2069388eb65750d715753e91b694c999bbc740ec
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-03-24 16:32:31 -0300

    Update firestore converter

--- src/utils/firestore-converter.ts
+++ src/utils/firestore-converter.ts
@@ -1,9 +1,8 @@
 import { QueryDocumentSnapshot } from "firebase/firestore";
-import { Video } from "../types/Video";
 
 export function firebaseConverter<T>() {
     return {
         toFirestore: (data: T) => data,
-        fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as Video
+        fromFirestore: (snap: QueryDocumentSnapshot) => snap.data() as T
     }
 }
