2978476aedab79502945bb30cb3aae2686273b46
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-03-25 13:41:43 -0300

    Update frebase.ts file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,21 +4,21 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const { 
-    VITE_API_KEY,
-    VITE_AUTH_DOMAIN,
-    VITE_PROJECT_ID,
-    VITE_STORAGE_BUCKET,
-    VITE_MESSAGING_SENDER_ID,
-    VITE_APP_ID,
-} = import.meta.env;
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID,
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId:  VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId:  MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
